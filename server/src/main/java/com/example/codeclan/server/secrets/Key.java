05390bbb1a76930ac330099711c34bb90c5edadd
Laura Dobie <moving.toyshop@gmail.com>
2021-02-18 13:57:30 +0000

    Initialised Spring.

---
+++ server/src/main/java/com/example/codeclan/server/secrets/Key.java
@@ -0,0 +1,15 @@
+package com.example.codeclan.server.secrets;
+
+public class Key {
+    private String key;
+    public Key(String key) {
+        this.key = "55547653";
+    }
+
+    public Key() {
+    }
+
+    public String getKey() {
+        return key;
+    }
+}

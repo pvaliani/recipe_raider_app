0c1bc6cd638af959fd6a2fb920c8041d6ac78cc8
Laura Dobie <moving.toyshop@gmail.com>
2021-02-18 15:31:09 +0000

    Commented out meal payload for now.

--- server/src/main/java/com/example/codeclan/server/models/MealPayload.java
+++ server/src/main/java/com/example/codeclan/server/models/MealPayload.java
@@ -1,9 +1,9 @@
-package com.example.codeclan.server.models;
-
-import java.util.List;
-
-public class MealPayload {
-    private List<Meal> meals;
-
-    public
-}
+//package com.example.codeclan.server.models;
+//
+//import java.util.List;
+//
+//public class MealPayload {
+//    private List<Meal> meals;
+//
+//    public
+//}

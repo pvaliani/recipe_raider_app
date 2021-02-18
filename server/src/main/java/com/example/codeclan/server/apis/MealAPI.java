f307d6821d053c69d4a0e5d44499b59d0cc9f1ba
Laura Dobie <moving.toyshop@gmail.com>
2021-02-18 15:54:32 +0000

    Put get meals code int MealApi, where it should be

--- server/src/main/java/com/example/codeclan/server/apis/MealAPI.java
+++ server/src/main/java/com/example/codeclan/server/apis/MealAPI.java
@@ -1,8 +1,22 @@
 package com.example.codeclan.server.apis;
 
+import com.example.codeclan.server.secrets.Key;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class MealAPI {
-    public
+    Key key;
+
+    public Object getMeals(String ingredients) {
+        key = new Key;
+        String apiKey = key.getKey();
+        String url = "https://themealdb.com/api/json/v2/" + apiKey +"/filter.php?i=" + ingredients;
+
+        RestTemplate restTemplate = new RestTemplate();
+        Object meals = restTemplate.getForObject(url, Object.class);
+
+        return meals;
+
+    }
 }
--- server/src/main/java/com/example/codeclan/server/controllers/MealController.java
+++ server/src/main/java/com/example/codeclan/server/controllers/MealController.java
@@ -7,17 +7,5 @@ import org.springframework.web.client.RestTemplate;
 
 
 public class MealController {
-    Key key;
 
-    public Object getMeals(String ingredients) {
-        key = new Key;
-        String apiKey = key.getKey();
-        String url = "https://themealdb.com/api/json/v2/" + apiKey +"/filter.php?i=" + ingredients;
-
-        RestTemplate restTemplate = new RestTemplate();
-        Object meals = restTemplate.getForObject(url, Object.class);
-
-        return meals;
-
-    }
 }

cae09c4d7822b38df1f632de18588eefddb9cfed
Laura Dobie <moving.toyshop@gmail.com>
2021-02-18 14:03:53 +0000

    Updated .gitignore.

--- server/src/main/java/com/example/codeclan/server/secrets/Key.java
+++ server/src/main/java/com/example/codeclan/server/secrets/Key.java
@@ -3,7 +3,7 @@ package com.example.codeclan.server.secrets;
 public class Key {
     private String key;
     public Key(String key) {
-        this.key = "55547653";
+        this.key = "66";
     }
 
     public Key() {

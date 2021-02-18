fb7db4218529fc4261ce2447a09d7b046c1b9345
Laura Dobie <moving.toyshop@gmail.com>
2021-02-18 14:15:56 +0000

    Created new folder

---
+++ server/src/main/java/com/example/codeclan/server/secrets/Key.java
@@ -0,0 +1,15 @@
+package com.example.codeclan.server.secrets;
+
+public class Key {
+    private String key;
+    public Key(String key) {
+        this.key = "66";
+    }
+
+    public Key() {
+    }
+
+    public String getKey() {
+        return key;
+    }
+}

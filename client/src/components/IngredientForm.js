5230a21841eb0fc122b60bd3549b3822b2990fa3
Pedram Valiani <p.valiani@gmail.com>
2021-02-20 15:58:32 +0000

    spring config added

--- client/src/components/IngredientForm.js
+++ client/src/components/IngredientForm.js
@@ -1,16 +1,34 @@
 import { Button, Form } from 'semantic-ui-react'
+import {useState} from 'react';
+
 
 // IngredientForm component takes up to 4 ingredients to render a meal
+// IngredientForm component takes in the handleIngredientSubmit as props from the mealContiner
 
-function IngredientForm() {
+function IngredientForm({handleIngredientSubmit}) {
+
+    // 
+    const [submittedData, setSubmittedData] = useState("");
+
+    const handleInput = (event) => {
+        setSubmittedData(event.target.value);
+        console.log(submittedData);
+
+        return submittedData;
+    }
+
+    const submitIngredients = (event) => {
+        event.preventDefault();
+        handleIngredientSubmit(submittedData);
+    }
     return (
         <>
             <Form className="form-box">
                 <Form.Field>
                 <label>Enter your ingredients</label>
-                <input placeholder='Enter up to 4 ingredients...' />
+                <input placeholder='Enter up to 4 ingredients...' onChange={handleInput} />
                 </Form.Field>
-                <Button type='submit'>Submit</Button>
+                <Button type='submit' onSubmit={submitIngredients}>Submit</Button>
             </Form>
 
         </>
--- client/src/containers/MealContainer.js
+++ client/src/containers/MealContainer.js
@@ -1,21 +1,46 @@
 import MealList from '../components/MealList';
 import IngredientForm from '../components/IngredientForm';
+import { useState, useEffect } from 'react';
 
 
 function MealContainer() {
+// Create useStates for the meals and ingredients from the search form
+// They will refresh app state in virtual dom upon an alteration to themselves
+const [meals, setMeals] = useState([]);
+const [ingredients, setIngredients] = useState("");
+
+const handleIngredientSubmit = (inputtedIngredients) => {
+    setIngredients(inputtedIngredients);
+}
+
+// This is the fetch which provides meals from the back end API via ingredients which will be input by the user
+const getMeals = (ingredients) => {
+    const url = "http://localhost:8080/api/meals/"+ingredients;
+    console.log("Url: " + url);
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            setMeals(data);
+        })
+}
+
+// useEffect re-runs the render every time there is a change to ingredients
+useEffect(() => {
+    getMeals(ingredients);
+}, [ingredients]);
+
+
+// Handler which deals with the ingredients input via the form and the state change as a result of the form submit
 
 
 
-// handleIngredientSubmit - handle the ingredient submission
-
-
-
-
-// render
+// Render - pass handleIngredient submit as props to the ingredient form component
 
 return(
     <>
-        <IngredientForm />
+        <IngredientForm handleIngredientSubmit={handleIngredientSubmit}/>
         <MealList />
     </>
 );

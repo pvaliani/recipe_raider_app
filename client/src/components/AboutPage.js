32dd5b32e5669a7172fbec3ca794619fb373b6f2
Kirsten Nicholson <kirstennic6@gmail.com>
2021-02-24 15:34:48 +0000

    about changes completedish

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -1,5 +1,7 @@
-import {Container, Header} from 'semantic-ui-react';
+import {Container, Header, Image} from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import PlateLogo from '../assets/PlateLogo.png';
+import CocktailLogo from '../assets/CocktailLogo.png';
 
 function AboutPage(){
     return(
@@ -14,12 +16,23 @@ function AboutPage(){
         <Header className='aboutHeader' textAlign="center"  as='h1'>
             About
         </Header>
-        <Container>
+        <Container className='aboutContainer'>
+            <br></br>
             <p>
-            Stuck in a recipe rut? Looking to expand your drinks repertoire? Make a Meal of It uses the MealDB API to bring back recipe suggestions based on up to 4 key ingredients, which will inspire you to get back in the kitchen! Make a Night of It does the same thing with the CocktailDB API. But for your drinks cabinet.
-Pedram, Laura and Kirsten made Make a Meal of It and Make a Night of it. You can check out our other projects at: insert GitHub links.
+            Stuck in a recipe rut? Looking to expand your drinks repertoire? Make a Meal of It uses the MealDB API to bring back recipe suggestions based on up to 4 key ingredients, which will inspire you to get back in the kitchen! 
             </p>
+            <p>Make a Night of It does the same thing with the CocktailDB API. But for your drinks cabinet.</p>
+            <p>
+                <a className="gitHub" href='https://github.com/pvaliani'> Pedram</a> , <a className="gitHub" href='https://github.com/lauracdobie'> Laura</a> and <a className="gitHub" href='https://github.com/Knichols02'>Kirsten </a> made Make a Meal of It and Make a Night of it. You can check out our other projects at: 
+                
+            </p>
+            <p className="aboutImages">
+                <Image className='aboutImages' src={PlateLogo} size='small' centered /> 
+                <Image className='aboutImages'src={CocktailLogo} size='small' centered /> 
+            </p>
+        
         </Container>
+        
         </>
     )
 }

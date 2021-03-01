768772ef5db26778602b8b40e4ff0db92a813d74
Laura Dobie <moving.toyshop@gmail.com>
2021-03-01 20:04:20 +0000

    Created test to check cache performs as expected.

--- server/src/test/java/com/example/codeclan/server/ServerApplicationTests.java
+++ server/src/test/java/com/example/codeclan/server/ServerApplicationTests.java
@@ -1,15 +1,23 @@
 package com.example.codeclan.server;
 
 import com.example.codeclan.server.apis.MealAPI;
+import com.example.codeclan.server.services.LRUCache;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest
 class ServerApplicationTests {
 
 	@Autowired
 	MealAPI mealAPI;
+
+	Map<String,String> testCache = new LRUCache<>(5);
 
 	@Test
 	void contextLoads() {
@@ -19,6 +27,19 @@ class ServerApplicationTests {
 	public void canCallApi(){
 		mealAPI.getMeals("chicken");
 		mealAPI.getRecipe("52772");
+	}
+
+	@Test
+	public void cacheOnlyRetainsFiveItems() {
+		testCache.put("Laura", "Seal");
+		testCache.put("Robert", "Duck");
+		testCache.put("Alan", "Beaver");
+		testCache.put("Sarah", "Otter");
+		testCache.put("Jenny", "Puffin");
+		testCache.put("Anna", "Owl");
+		assertEquals(5, testCache.size());
+		assertFalse(testCache.containsKey("Laura"));
+
 	}
 
 }

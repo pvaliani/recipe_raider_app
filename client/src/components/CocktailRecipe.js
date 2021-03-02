45683f055dee22561b0a354dcb0b6660030d105b
Laura Dobie <moving.toyshop@gmail.com>
2021-03-02 14:50:48 +0000

    refactored cocktail recipe if statement

--- client/src/components/CocktailRecipe.js
+++ client/src/components/CocktailRecipe.js
@@ -13,80 +13,17 @@ function CocktailRecipe({cocktailRecipe}) {
     let ingredientsList = []
   
     const displayIngredients = () => {
-      // Object.keys(cocktailRecipe)
-      // let text = cocktailRecipe["strMeasure1"] 
-      // console.log(text);
-      // delete cocktailRecipe["strMeasure1"]
-      // console.log(cocktailRecipe);
+      let values = []
 
-      if((cocktailRecipe.strMeasure1 !== null && cocktailRecipe.strMeasure1 !== "") && (cocktailRecipe.strIngredient1 !== null && cocktailRecipe.strIngredient1 !== "")) {
-        const ingredientEntry = `${cocktailRecipe.strMeasure1} ${cocktailRecipe.strIngredient1}`;
-        ingredientsList.push(ingredientEntry);
-      }
-      if((cocktailRecipe.strMeasure2 !== null && cocktailRecipe.strMeasure2 !== "") && (cocktailRecipe.strIngredient2 !== null && cocktailRecipe.strIngredient2 !== "")) {
-        const ingredientEntry = `${cocktailRecipe.strMeasure2} ${cocktailRecipe.strIngredient2}`;
-        ingredientsList.push(ingredientEntry);
-      }
-      if((cocktailRecipe.strMeasure3 !== null && cocktailRecipe.strMeasure3 !== "") && (cocktailRecipe.strIngredient3 !== null && cocktailRecipe.strIngredient3 !== "")) {
-        const ingredientEntry = `${cocktailRecipe.strMeasure3} ${cocktailRecipe.strIngredient3}`;
-        ingredientsList.push(ingredientEntry);
-      }
-      if((cocktailRecipe.strMeasure4 !== null && cocktailRecipe.strMeasure4 !== "") && (cocktailRecipe.strIngredient4 !== null && cocktailRecipe.strIngredient4 !== "")) {
-        const ingredientEntry = `${cocktailRecipe.strMeasure4} ${cocktailRecipe.strIngredient4}`;
-        ingredientsList.push(ingredientEntry);
-      }
-  
-      if((cocktailRecipe.strMeasure5 !== null && cocktailRecipe.strMeasure5 !== "") && (cocktailRecipe.strIngredient5 !== null && cocktailRecipe.strIngredient5 !== "")) {
-        const ingredientEntry = `${cocktailRecipe.strMeasure5} ${cocktailRecipe.strIngredient5}`;
-        ingredientsList.push(ingredientEntry);
-      }
-      if((cocktailRecipe.strMeasure6 !== null && cocktailRecipe.strMeasure6 !== "") && (cocktailRecipe.strIngredient6 !== null && cocktailRecipe.strIngredient6 !== "")) {
-        const ingredientEntry = `${cocktailRecipe.strMeasure6} ${cocktailRecipe.strIngredient6}`;
-        ingredientsList.push(ingredientEntry);
-      }
-      if((cocktailRecipe.strMeasure7 !== null && cocktailRecipe.strMeasure7 !== "") && (cocktailRecipe.strIngredient7 !== null && cocktailRecipe.strIngredient7 !== "")) {
-        const ingredientEntry = `${cocktailRecipe.strMeasure7} ${cocktailRecipe.strIngredient7}`;
-        ingredientsList.push(ingredientEntry);
-      }
-      if((cocktailRecipe.strMeasure8 !== null && cocktailRecipe.strMeasure8 !== "") && (cocktailRecipe.strIngredient8 !== null && cocktailRecipe.strIngredient8 !== "")) {
-        const ingredientEntry = `${cocktailRecipe.strMeasure8} ${cocktailRecipe.strIngredient8}`;
-        ingredientsList.push(ingredientEntry);
-      }
-  
-      if((cocktailRecipe.strMeasure9 !== null && cocktailRecipe.strMeasure9 !== "") && (cocktailRecipe.strIngredient9 !== null && cocktailRecipe.strIngredient9 !== "")) {
-        const ingredientEntry = `${cocktailRecipe.strMeasure9} ${cocktailRecipe.strIngredient9}`;
-        ingredientsList.push(ingredientEntry);
-      }
-      if((cocktailRecipe.strMeasure10 !== null && cocktailRecipe.strMeasure10 !== "") && (cocktailRecipe.strIngredient10 !== null && cocktailRecipe.strIngredient10 !== "")) {
-        const ingredientEntry = `${cocktailRecipe.strMeasure10} ${cocktailRecipe.strIngredient10}`;
-        ingredientsList.push(ingredientEntry);
-      }
-      if((cocktailRecipe.strMeasure11 !== null && cocktailRecipe.strMeasure11 !== "") && (cocktailRecipe.strIngredient11 !== null && cocktailRecipe.strIngredient11 !== "")) {
-        const ingredientEntry = `${cocktailRecipe.strMeasure11} ${cocktailRecipe.strIngredient11}`;
-        ingredientsList.push(ingredientEntry);
-      }
-      if((cocktailRecipe.strMeasure12 !== null && cocktailRecipe.strMeasure12 !== "") && (cocktailRecipe.strIngredient12 !== null && cocktailRecipe.strIngredient12 !== "")) {
-        const ingredientEntry = `${cocktailRecipe.strMeasure12} ${cocktailRecipe.strIngredient12}`;
-        ingredientsList.push(ingredientEntry);
-      }
-  
-      if((cocktailRecipe.strMeasure13 !== null && cocktailRecipe.strMeasure13 !== "") && (cocktailRecipe.strIngredient13 !== null && cocktailRecipe.strIngredient13 !== "")) {
-        const ingredientEntry = `${cocktailRecipe.strMeasure13} ${cocktailRecipe.strIngredient13}`;
-        ingredientsList.push(ingredientEntry);
-      }
-      if((cocktailRecipe.strMeasure14 !== null && cocktailRecipe.strMeasure14 !== "") && (cocktailRecipe.strIngredient14 !== null && cocktailRecipe.strIngredient14 !== "")) {
-        const ingredientEntry = `${cocktailRecipe.strMeasure14} ${cocktailRecipe.strIngredient14}`;
-        ingredientsList.push(ingredientEntry);
-      }
-      if((cocktailRecipe.strMeasure15 !== null && cocktailRecipe.strMeasure15 !== "") && (cocktailRecipe.strIngredient15 !== null && cocktailRecipe.strIngredient15 !== "")) {
-        const ingredientEntry = `${cocktailRecipe.strMeasure15} ${cocktailRecipe.strIngredient15}`;
-        ingredientsList.push(ingredientEntry);
-      }
-     
+      Object.keys(cocktailRecipe).forEach((k, i) => { 
+        values.push(cocktailRecipe[k]) });
       
-      // console log the ingredients list to check it in the browser
-      console.log(ingredientsList);
-  
+      for (let i = 32; i < values.length - 4; i++) { 
+        if (values[i] !== null && values[i] !== "") { 
+          const element = values[i] + " " + values[i-15]; 
+          ingredientsList.push(element); } 
+        }
+
       return ingredientsList;
   
     }
